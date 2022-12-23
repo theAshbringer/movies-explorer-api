dc72f1e75d0fdd9fcf7b5f1b8b6de31079a3c334
Kristina Dzhumanova <dzhumanovakd@iss-reshetnev.ru>
2022-12-23 10:15:58 +0300

    feat: add loggers

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const routes = require('./routes');
 const { MSG_ROUTE_NOT_FOUND } = require('./utils/constants');
 const NotFoundError = require('./errors/not-found-err');
 const errorHandler = require('./middlewares/errorHandler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { corsOptions } = require('./middlewares/cors');
 
 require('dotenv').config();
@@ -23,8 +24,10 @@ app.use(cookieParser());
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
+app.use(requestLogger);
 app.use(routes);
 
+app.use(errorLogger);
 app.use(errors()); // handling Joi errors
 
 app.use((req, res, next) => next(new NotFoundError(MSG_ROUTE_NOT_FOUND)));
